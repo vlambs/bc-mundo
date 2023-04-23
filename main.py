d0149f5e49335fff9ad42efd71b8bb4bd2e497c8
vlambs <ryobudev@gmail.com>
2023-04-23 16:18:02 +0200

    Routing each page

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-from bottle import route, run
+from bottle import route, run, view
 
 @route('/')
+@view("templates/default.tpl")
 def default():
-    return "This is the default web page of bc-mundo."
+    return {"title":"Default"}
 
-@route('/hello')
-def hello():
-    return "Hello World."
+@route('/club')
+@view("templates/club.tpl")
+def default():
+    stri = """
+    <h2>club</h2>
+    """
+    return {"title":"Club", "body" : stri}
 
-@route('/hello/<nom>')
-def hello(nom):
-    return "Hello World " + nom
+@route('/contact')
+@view("templates/contact.tpl")
+def default():
+    stri = """
+    <h2>contact</h2>
+    """
+    return {"title":"Contact", "body" : stri}
+
+@route('/media')
+@view("templates/media.tpl")
+def default():
+    stri = """
+    <h2>media</h2>
+    """
+    return {"title":"Media", "body" : stri}
+
+@route('/news')
+@view("templates/news.tpl")
+def default():
+    stri = """
+    <h2>news</h2>
+    """
+    return {"title":"News", "body" : stri}
+
+@route('/teams')
+@view("templates/teams.tpl")
+def default():
+    stri = """
+    <h2>teams</h2>
+    """
+    return {"title":"Teams", "body" : stri}
+
 
 run(host='localhost', port=8080, debug=True)
